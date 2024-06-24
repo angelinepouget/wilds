687885b88bb5dedf119537f6a246b21dadc0794a
Angéline Pouget <angeline.pouget@pouget.ch>
2024-06-24 10:43:10 -0400

    import changes

--- examples/models/initializer.py
+++ examples/models/initializer.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import os
 import traceback
 
-from models.layers import Identity
-from utils import load
+from examples.models.layers import Identity
+from examples.utils import load
 
 def initialize_model(config, d_out, is_featurizer=False):
     """
